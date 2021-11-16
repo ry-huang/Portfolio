a2257cf43b4f3b01faa5ed3d12390399ae3600d2
Linda <lindathehuang@gmail.com>
2021-11-15 23:32:18 -0800

    Added Prelim Portfolio Files

---
+++ stylesheets/script.js
@@ -0,0 +1,85 @@
+$(document).ready(function(){
+
+    checkWidth();
+    $(window).resize(function() { checkWidth(); })
+    function checkWidth() {
+        if ($(window).width() < 1200) {
+            $("body").addClass("mobile");
+        } else {
+            $("body").removeClass("mobile");
+            $(".mobileMenu").css({ "transition": "none" })
+        }
+    }
+
+    $(".triplebar").click(function() {
+        $(".mobileMenu").css({ "transition": "all 1s ease-out" })
+        $(".mobileMenu").toggleClass("expanded");;
+    });
+
+    $("#currentYear").html(new Date().getFullYear());
+
+
+
+    // Hero Fix ----------------------------------------------------------------------
+    var conHeight = $("#pullup .hero").height();
+    var imgHeight = $("#pullup .hero img").height();
+    var gap = (imgHeight - conHeight) / 2;
+    $("#pullup .hero img").css("top", -gap);
+
+
+
+    // Pullup ------------------------------------------------------------------------
+    $(".projectContainer").click(function() { 
+        /* Get Project ID */
+        const PROJECTINDEX = $(this).index();
+        const PROJECTID = $(".projectContainer:nth-child("+(PROJECTINDEX+1)+")").attr("id");
+
+        /* Project Hero Image Setup */
+        const PROJECTIMGURL = $("#"+(PROJECTID)+" img").attr("src")
+        const PROJECTHERO = "url("+PROJECTIMGURL+")";
+        $("#pullupProject .hero").css({ "background":PROJECTHERO, "background-position":"50% 50%" });
+
+        /* Project Name Setup */
+        const PNAME = "#"+(PROJECTID)+" p:first-of-type";
+        const PROJECTNAME = $(PNAME).text();
+        $("#pullupProject .hero h1").html(PROJECTNAME);
+        const PSUB = "#"+(PROJECTID)+" p:nth-child(2)";
+        const PSUBNAME = $(PSUB).text();
+        $("#pullupProject .hero p").html(PSUBNAME);
+
+        /* Project Content Setup */
+        const PROJECTDESC = "projectfiles/" + (PROJECTID) + ".html"
+        $("#pullupProject .fullDescription").load(PROJECTDESC); 
+
+        setPullup("project");
+    });
+
+    $("#about").click(function() { 
+        $("#pullupAbout .hero").css({ "background": "url(images/about/banner-about.jpg)"});
+        setPullup("about");
+    });
+
+    $("#photography").click(function() {
+        $("#pullupProject .fullDescription").load("projectfiles/photography.html"); 
+        $("#pullupAbout .hero").css({ "background": "url(images/about/banner-about.jpg)"});
+        setPullup("photography");
+    });
+
+    function setPullup(type) {
+        if(type === "project" || type === "photography") {
+            $("#pullupProject").addClass("show");
+            $("#pullupAbout").removeClass("show");
+        } else { //about
+            $("#pullupAbout").addClass("show");
+            $("#pullupProject").removeClass("show");
+        } 
+        $("#pullup").addClass("show"); 
+    }
+
+    $("#pullupToggle").click(function() { hidePullup() });
+    $("#design").click(function() { hidePullup() });
+    $("#logo").click(function() { hidePullup() });
+    function hidePullup(){
+        $("#pullup").removeClass("show");
+    }
+})
